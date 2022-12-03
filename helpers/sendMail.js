bd597fc23f2c1e3681f15c064d027f5f3089f6f9
Yaroslav Shtanko <81259415+Yaroslav1918@users.noreply.github.com>
2022-12-03 23:45:38 +0200

    .

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -2,12 +2,10 @@ const sgMail = require("@sendgrid/mail");
 require("dotenv").config();
 
 const { SENDGRID_API_KEY } = process.env;
-
 sgMail.setApiKey(SENDGRID_API_KEY);
-
 const sendMail = async (data) => {
   try {
-    const mail = { ...data, from: "bogdan.lyamzin.d@gmail.com" };
+    const mail = { ...data, from: "zyxmen1918@gmail.com" };
     await sgMail.send(mail);
     return true;
   } catch (error) {
--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const mongoose = require("mongoose");
 const app = require("./app");
 require("dotenv").config();
 const { DB_HOST } = process.env;
-console.log(DB_HOST);
-const MONGO_URI = "https://git.heroku.com/tic-tac-toe-game.git";
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(MONGO_URI, () => {
+    app.listen(3038, () => {
       console.log("Server running. Use our API on port: 3038");
     });
   })
