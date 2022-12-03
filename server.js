2b051ba6fbcd72ed7b76e7176b46ad5f9b12dc40
Yaroslav Shtanko <81259415+Yaroslav1918@users.noreply.github.com>
2022-12-03 19:13:04 +0200

    .

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const app = require("./app");
 require("dotenv").config();
 const { DB_HOST } = process.env;
 
-const KEY =
-  "mongodb+srv://Yaroslav:AIGWoHhd4rvtoq1K@atlascluster.y3krkt5.mongodb.net/Tic-tac-toe?retryWrites=true&w=majority";
 mongoose
-  .connect(KEY)
+  .connect(DB_HOST)
   .then(() => {
     app.listen(3038, () => {
       console.log("Server running. Use our API on port: 3038");
